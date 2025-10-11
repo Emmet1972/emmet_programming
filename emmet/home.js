2f67a5207c89f6c0b5f71458a48e3fe8a80c5919
EMMET TRAORE JR <83843055+Emmet1972@users.noreply.github.com>
2025-10-11 10:20:58 -0700

    Add files via upload

---
+++ emmet/home.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+let slideTimer;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].classList.add("active");
+    dots[slideIndex-1].className += " active";
+    clearTimeout(slideTimer);
+    slideTimer = setTimeout(function() { plusSlides(1); }, 4000); // Change image every 4 seconds
+}
